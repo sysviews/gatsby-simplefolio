915005101232545dd0ad0447c14ee332cfe0d77b
sysviews <43782059+sysviews@users.noreply.github.com>
2021-02-21 16:40:18 +0530

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,26 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Prasanth | Data Engineer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello, my name is',
+  name: 'Prasanth',
+  subtitle: 'Assistant Consultant @ TCS',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'ProfilePrasanth.jpg',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,29 +29,38 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'Data Management Platform',
+    info: 'Build Framework to automate the processes of Data Ingestion, Processing and Metric Generation. Reduce the Time on Data Engineering',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'Channel Data Management',
+    info: 'Simplify the process of Field Sales Invoices and identify ways for accurate reporting,bridging gap between Sales and Finance Teams.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
+    title: 'Devices And Insights',
+    info: 'Analyse usage pattern of applications,devices by end-users in an Digital First Organization.',
+    info2: ' Provide Insights to provide safe on-boarding and ',
+    url: 'https://www.microsoft.com/en-us/itshowcase/reducing-friction-throughout-the-device-lifecycle-at-microsoft',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Bot Experiences',
+    info: 'Automating End User Queries using technologies like BotFramework,Microsoft LUIS.Build Self-Service ChatBuilder with capabilities to build ChatBots without IT Support by End User',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +68,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'prasanth7@live.in',
 };
 
 // FOOTER DATA
@@ -68,7 +77,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/prashanth010',
     },
     {
       id: nanoid(),
@@ -78,7 +87,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/prasanth-perumalla-89001b49/',
     },
     {
       id: nanoid(),
@@ -90,5 +99,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
